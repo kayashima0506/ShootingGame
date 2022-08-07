5cc90dc90935701dd34a749b0b9a03980fe1f9eb
{kayashima} <{kayashima0506@gmail.com}>
2022-08-07 15:00:27 +0900

    敵を倒すところまで

---
+++ src/io/github/Shooting/Bullet.java
@@ -0,0 +1,19 @@
+package io.github.Shooting;
+
+/*
+ * 弾のX,Y座標を管理
+ */
+public class Bullet {
+    public int x;
+    public int y;
+
+    public Bullet() {
+    }
+
+    public Bullet(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
---
+++ src/io/github/Shooting/Enemy.java
@@ -0,0 +1,14 @@
+package io.github.Shooting;
+
+public class Enemy {
+    public int x;
+    public int y;
+
+    public Enemy() {
+    }
+
+    public Enemy(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
